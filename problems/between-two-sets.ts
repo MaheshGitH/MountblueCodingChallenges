320a09a900a40af63502e4530b09db603e5eb13a
Mahesh Krishnan <maheshkrishnan1242@gmail.com>
2026-04-14 15:06:24 +0530

    solve: between-two-sets

---
+++ problems/between-two-sets.ts
@@ -0,0 +1,33 @@
+//Helper functions
+
+function getGcd(a: number, b: number): number {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function getLcm(a: number, b: number): number {
+  if (a === 0 || b === 0) return 0;
+  return Math.abs(a * b) / getGcd(a, b);
+}
+
+//Main
+
+function getTotalX(a: number[], b: number[]): number {
+  let result = 0;
+  const lcm = a.reduce(getLcm);
+  const gcd = b.reduce(getGcd);
+  let multiple = 0;
+  while (multiple <= gcd) {
+    multiple += lcm;
+    if (gcd % multiple === 0) {
+      result++;
+    }
+  }
+  return result;
+}
+
+console.log(getTotalX([2, 4], [16, 32, 96]));
+
+// //Time complexity: O((n + m) * log M)
