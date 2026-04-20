3058ca466b14424348074ff06af44a9c50581773
Mahesh Krishnan <maheshkrishnan1242@gmail.com>
2026-04-21 00:16:59 +0530

    solve: drawing-book

--- problems/1-20/drawing-book.ts
+++ problems/1-20/drawing-book.ts
@@ -1,9 +1,23 @@
 /*
-n = 10
-p = 8
-
+frontPage = Math.floor(p / 2)
+backPage = 0
+if(n % 2 === 0) {
+  backPage = Math.floor((n - p + 1) / 2)
+} else {
+   backPage = Math.floor((n - p) / 2)
+}
+return frontPage < backPage ? fronPage : backPage
 */
 
 function pageCount(n: number, p: number): number {
-  return;
+  const fronPage = Math.floor(p / 2);
+  let backPage = 0;
+  if (n % 2 === 0) {
+    backPage = Math.floor((n - p + 1) / 2);
+  } else {
+    backPage = Math.floor((n - p) / 2);
+  }
+  return fronPage < backPage ? fronPage : backPage;
 }
+
+console.log(pageCount(5, 4));
