d81ef538d3d3490c5d827bcd916a13ff35269bef
Mahesh Krishnan <maheshkrishnan1242@gmail.com>
2026-05-24 17:35:09 +0530

    solve: sherlock and the valid string

--- problems/120-122/sherlock-and-the-val-str.ts
+++ problems/120-122/sherlock-and-the-val-str.ts
@@ -1,55 +1,51 @@
+/*
+s = "aaaabbcccccc"
+arr = [4, 2, 6]
+firstNum = arr[0]
+expected = firstNum + 1 | 1 
+for loop -> {
+  if(arr[i] === firstNum) {
+    continue
+  }
+  if(arr[i] === expected) {
+    expected = firstNum
+    continue
+  }
+  return "NO"
+}
+return "YES"
+*/
+
 function isValid(s: string): string {
   const countOccurrence = new Map<string, number>();
-  let max = 0;
   for (const char of s) {
     countOccurrence.set(
       char,
       countOccurrence.get(char) ? countOccurrence.get(char) + 1 : 1,
     );
-    if (countOccurrence.get(char) > max) {
-      max = countOccurrence.get(char);
-    }
   }
 
   const freqs = [...countOccurrence.values()];
-  freqs.sort((a, b) => a - b);
-  console.log(freqs);
-  let maxCount = 1;
-  let currentCount = 1;
-  let mostFreq = 0;
-  for (let i = 1; i < freqs.length; i++) {
-    if (freqs[i] === freqs[i - 1]) {
-      currentCount++;
-    } else {
-      currentCount = 1;
-    }
-    if (currentCount > maxCount) {
-      maxCount = currentCount;
-      mostFreq = freqs[i];
-    }
-  }
-  console.log(mostFreq);
-  const value = max - mostFreq;
-  let expected = 0;
-  if (value === 0) {
-    expected = max - 1;
-  } else if (value === 1) {
-    expected = max;
-  } else {
-    return "NO";
-  }
 
-  for (const fre of freqs) {
-    if (fre === expected) {
-      expected = mostFreq;
+  const firstNum = freqs[0];
+  let expected = firstNum;
+  let hasPass = true;
+  for (const freq of freqs) {
+    if (freq === firstNum) {
       continue;
     }
-    if (fre !== mostFreq) {
-      return "NO";
+    if (hasPass) {
+      if (freq === expected + 1 || freq === expected - 1 || freq === 1) {
+        expected = firstNum;
+      }
+      hasPass = false;
+      continue;
     }
+
+    return "NO";
   }
 
   return "YES";
 }
 
-console.log(isValid("aaaabbcccccc"));
+console.log(isValid("aabbcd"));
