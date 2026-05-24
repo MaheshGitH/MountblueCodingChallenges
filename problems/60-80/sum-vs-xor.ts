cdb598325eb142d706faf456fd4550a1dffb797f
Mahesh Krishnan <maheshkrishnan1242@gmail.com>
2026-05-24 16:36:35 +0530

    solve: sum vs xor

---
+++ problems/60-80/sum-vs-xor.ts
@@ -0,0 +1,29 @@
+/*
+n = 4
+count = 0
+for loop -> {
+  addition = n + i
+  xor = n ^ i
+  if(addition === xor) {
+    count++
+  }
+}
+*/
+
+function sumXor(n: number): number {
+  if (n === 0) return 1;
+  let count = 0;
+  const bin = n.toString(2);
+
+  for (const char of bin) {
+    if (char === "0") {
+      count++;
+    }
+  }
+
+  return 2 ** count;
+}
+
+console.log(sumXor(1));
+
+console.log(1 ^ 1);
